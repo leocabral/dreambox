a25f2328a45cf7be89a0024a05f11fb21fc4c038
Matheus Martins do Rego <matheus.martins@dls158.lab.dextra.com.br>
2016-01-29 21:53:54 -0200

    Randomizando nuvens geradas

--- frontend/main.js
+++ frontend/main.js
@@ -1,14 +1,23 @@
-
-
-var clouds = 5;
-var i=1;
+var clouds = 0;
 var cloudCreator = setInterval(function() {
-	$('#clouds').append('<div class=" cloud x'+i+'"></div>');
+	var cloud = $('<div class=" cloud"></div>').appendTo('#clouds');
+	var top = Math.floor((Math.random() * 8)) * 5;
+	var moveSpeed = Math.floor((Math.random() * 3) + 6) * 3;
+	var scale = Math.floor((Math.random() * 3) + 6) / 10;
+	cloud.css({
+		'top' : top + '%',
+		'-webkit-transform' : 'scale(' + scale + ')',
+		'-moz-transform' : 'scale(' + scale + ')',
+		'transform' : 'scale(' + scale + ')',
+		'opacity' : scale,
+		'-webkit-animation' : 'moveclouds ' + moveSpeed + 's linear infinite',
+		'moz-animation' : 'moveclouds ' + moveSpeed + 's linear infinite',
+		'-o-animation' : 'moveclouds ' + moveSpeed + 's linear infinite'
+	});
+	
 	i++;
 	clouds++;
-	if (i==6)
-		i = 1;
-	if (clouds == 35)
+	if (clouds == 10)
 		clearInterval(cloudCreator);
 }, 1000);
 
