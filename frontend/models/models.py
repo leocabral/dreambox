36ac9efccaf1c484e60417be4eec8022b7bac3ab
TomasSQ <tomasqueiroga@gmail.com>
2016-01-29 23:22:16 -0200

    model description

--- frontend/models/models.py
+++ frontend/models/models.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import ndb
 
-class Author(ndb.Model):
-	"""Sub model for representing an author."""
-	identity = ndb.StringProperty(indexed=False)
-	email = ndb.StringProperty(indexed=False)
+class Dream(ndb.Model):
+	"""Sub model for representing an Dream."""
+	name = ndb.StringProperty(indexed=True)
+	description = ndb.StringProperty(indexed=False)
