a8c3dd0b77177990ec0adfe0a4535e314330b91b
TomasSQ <tomasqueiroga@gmail.com>
2016-01-29 22:35:10 -0200

    run on gae

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#cd ~/ && wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.31.zip && unzip google_appengine_1.9.31.zip
 ~/google_appengine/dev_appserver.py --port=1234  web/
