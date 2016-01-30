dd84f18fefb69db92e0cb53dcbb78619c9e8e26a
vepo <victor.perticarrari@gmail.com>
2016-01-30 12:31:54 -0200

    Adicionando funcionalidade de Signin/Signup

--- frontend/static/js/controllers/signin-controller.js
+++ frontend/static/js/controllers/signin-controller.js
@@ -1,3 +1,15 @@
-angular.module('dream-box').controller('SigninController', function(){
-	console.log(this);
-});
+angular.module('dream-box').controller('SigninController', ['$scope', '$http', function($scope, $http){
+	var vm = this;
+	vm.credentials = {
+		username : '',
+		password : ''
+	};
+
+	vm.login = function() {
+		$http.post('/api/helpers', vm.credentials).then(function(){
+			console.log(arguments);
+		}, function(){
+			console.log(arguments);
+		});
+	};
+}]);
--- frontend/static/js/controllers/signup-controller.js
+++ frontend/static/js/controllers/signup-controller.js
@@ -1,3 +1,16 @@
-angular.module('dream-box').controller('SignupController', function(){
-	console.log(this);
-});
+angular.module('dream-box').controller('SignupController', ['$http', function($http){
+	var vm = this;
+	vm.newUser = {
+		name : '',
+		last_name : '',
+		email : '',
+		password : ''
+	};
+	vm.signup = function() {
+		$http.post('/api/helpers', vm.newUser).then(function(){
+			console.log(arguments);
+		}, function(){
+			console.log(arguments);
+		});
+	};
+}]);
