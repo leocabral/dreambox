d27034e959c71c41f26c1562c2f4d719c9431172
William Golcalvez da Cruz <william.cruz@dls101.lab.dextra.com.br>
2016-01-29 23:24:51 -0200

    DreamerAcitivity button to home

---
+++ mobile/app/src/main/java/br/com/dreambox/activity/DreamerAcitivity.java
@@ -0,0 +1,23 @@
+package br.com.dreambox.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.dreambox.R;
+import br.com.dreambox.activity.HomeActivity;
+import butterknife.OnClick;
+
+public class DreamerAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dreamer_acitivity);
+    }
+
+    @OnClick(R.id.button_create_dreamer)
+    public void createDreamer() {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+}
