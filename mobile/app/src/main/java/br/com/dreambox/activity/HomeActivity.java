70353a0ab1b026456353118be354832715640b4e
Leonardo Cabral <leonardocabral96@gmail.com>
2016-01-29 22:16:10 -0200

    sign up

--- mobile/app/src/main/java/br/com/dreambox/activity/HomeActivity.java
+++ mobile/app/src/main/java/br/com/dreambox/activity/HomeActivity.java
@@ -1,4 +1,4 @@
-package br.com.dreambox.dreambox.activity;
+package br.com.dreambox.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -6,11 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 
 import br.com.dreambox.dreambox.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_home);
     }
 }
