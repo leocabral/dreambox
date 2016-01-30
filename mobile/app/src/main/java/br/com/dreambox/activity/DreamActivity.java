c14956732413a75f59939ae9046e3311cdc5217d
Joao Vendrame <jvgengo@gmail.com>
2016-01-29 22:39:54 -0200

    Clicked createDream start ProfileActivity

--- mobile/app/src/main/java/br/com/dreambox/activity/DreamActivity.java
+++ mobile/app/src/main/java/br/com/dreambox/activity/DreamActivity.java
@@ -1,17 +1,25 @@
 package br.com.dreambox.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import br.com.dreambox.R;
+import butterknife.OnClick;
 
 public class DreamActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dream);
     }
+
+    @OnClick(R.id.create_dream_button)
+    protected void createDream() {
+        startActivity(new Intent(this, ProfileActivity.class));
+    }
+
+
 
 }
