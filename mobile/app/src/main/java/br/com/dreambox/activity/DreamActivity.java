015dcb5be7394d357e868ef6a30fa9cedf1dc5ea
Joao Vendrame <jvgengo@gmail.com>
2016-01-30 00:21:42 -0200

    Added tags on dreamActivity

--- mobile/app/src/main/java/br/com/dreambox/activity/DreamActivity.java
+++ mobile/app/src/main/java/br/com/dreambox/activity/DreamActivity.java
@@ -1,18 +1,45 @@
 package br.com.dreambox.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.dreambox.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
+import co.lujun.androidtagview.TagContainerLayout;
+import co.lujun.androidtagview.TagView;
 
 public class DreamActivity extends AppCompatActivity {
+
+    @Bind(R.id.dream_tag_container)
+    public TagContainerLayout tagContainerLayout;
+
+    @Bind(R.id.dream_text_tag)
+    public EditText textTag;
+
+    @Bind(R.id.dream_add_tag)
+    public Button btnAddTag;
+
+    public List<String> listTag = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dream);
+        ButterKnife.bind(this);
+
+        setupTagGroup();
     }
 
     @OnClick(R.id.create_dream_button)
@@ -21,5 +48,43 @@ public class DreamActivity extends AppCompatActivity {
     }
 
 
+    public void setupTagGroup() {
+        tagContainerLayout.setOnTagClickListener(new TagView.OnTagClickListener() {
+            @Override
+            public void onTagClick(int position, String text) {
+                Toast.makeText(DreamActivity.this, "click-position:" + position + ", text:" + text,
+                        Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onTagLongClick(final int position, String text) {
+                AlertDialog dialog = new AlertDialog.Builder(DreamActivity.this)
+                        .setTitle("long click")
+                        .setMessage("You will delete this tag!")
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                tagContainerLayout.removeTag(position);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
+
+        tagContainerLayout.setTags(listTag);
+
+        btnAddTag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tagContainerLayout.addTag(textTag.getText().toString());
+            }
+        });
+    }
 }
