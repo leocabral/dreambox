227ab250048a65d863be5b8cceabf84f9223a71a
TomasSQ <tomasqueiroga@gmail.com>
2016-01-30 01:55:54 -0200

    dreamers

--- frontend/models/dreamers.py
+++ frontend/models/dreamers.py
@@ -1,17 +1,18 @@
 import json
 import cgi
+import webapp2
 
 from google.appengine.ext import ndb
 from google.appengine.ext import db
 
 from lib import ndb_json
-import webapp2
+from dateutil.parser import parse
 
 class Dreamers(ndb.Model):
     name = ndb.StringProperty(indexed=True)
-    lastname = ndb.StringProperty(indexed=True)
+    last_name = ndb.StringProperty(indexed=True)
     birthday = ndb.DateProperty(indexed=True)
-    dreamingSince = ndb.DateTimeProperty(auto_now_add=True)
+    dreaming_since = ndb.DateTimeProperty(auto_now_add=True)
 
     @classmethod
     def find_all(cls):
@@ -23,7 +24,11 @@ class DreamersAPI(webapp2.RequestHandler):
         self.response.out.write(ndb_json.dumps(Dreamers.find_all()))
 
     def post(self):
-        dreamer = Dreamers(name = self.request.get('name'), lastname = self.request.get('lastname'), birthday = self.request.get('birthday'))
+        birthday = None
+        if self.request.get('birthday') != None :
+            birthday = parse(self.request.get('birthday'))
+
+        dreamer = Dreamers(name = self.request.get('name'), last_name = self.request.get('last_name'), birthday = birthday)
 
         self.response.out.write(ndb_json.dumps(dreamer.put().get()))
 
