8c43832d7aac199be143edbbafa8df6ed284e153
Vinicus Rodrigues Moreti <vinicius.moreti@dls158.lab.dextra.com.br>
2016-01-30 09:06:46 -0200

    refactoring js

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -3,88 +3,9 @@ $('img.robot').unbind('click').click(function(e) {
     e.stopPropagation();
 });
 
-var clouds = 0;
-var createCloud = function () {
-	var cloud = $('<div class=" cloud"></div>').appendTo('#clouds');
-	var top = Math.floor((Math.random() * 8)) * 5;
-	var moveSpeed = Math.floor((Math.random() * 3) + 6) * 3;
-	var scale = Math.floor((Math.random() * 3) + 6) / 10;
-	cloud.css({
-		'top' : top + '%',
-		'-webkit-transform' : 'scale(' + scale + ')',
-		'-moz-transform' : 'scale(' + scale + ')',
-		'transform' : 'scale(' + scale + ')',
-		'opacity' : scale,
-		'-webkit-animation' : 'moveclouds ' + moveSpeed + 's linear infinite',
-		'-moz-animation' : 'moveclouds ' + moveSpeed + 's linear infinite',
-		'-o-animation' : 'moveclouds ' + moveSpeed + 's linear infinite'
-	});
-	
-	clouds++;
-}
-var buildCreator = function (interval) {
-	var creator = setInterval(function() {
-		createCloud();
-		if (clouds >= 50) {
-			clearInterval(creator);
-		}
-	}, interval);
-	return creator;
-};
-createCloud();
-buildCreator(800);
-buildCreator(1000);
-buildCreator(1200);
-
-var openCloud = function(cloud, html) {
-	cloud.animate({
-		left : ($(window).width()/2-$(cloud).width()/2)+'px',
-	}, 40);
-	cloud.css({
-		'-webkit-animation': 'none',
-		'-moz-animation': 'none',
-		'-o-animation': 'none',
-		'animation' : 'none'
-	});
-	cloud.addClass('active-cloud');
-	cloud.html(html);
-
-	$('.follow').unbind('click').click(function() {
-		var button = $(this);
-		var id = button.data('id');
-//		$.ajax({
-//			method: 'PUT',
-//			url: 'some.php',
-//			data: { 'id' : id }
-//		}).done(function() {
-		    button.prop('disabled', true);
-//        });
-	});
-	unbindClouds();
-};
-
-var applyCloudTemplate = function(cloud, dream) {
-	$.get('templates/opened-dream.html').done(function(template) {
-		openCloud(cloud, doT.template(template)( {
-			'dream' : dream,
-			'origin' : window.location.origin
-		}));
-	});
-};
-
-var bindClouds = function() {
-	$('#clouds').on('click', '> div', function(e) {
-		var cloud = $(this);
-		$.getJSON('/api/dreams/random').success(function(dream) {
-			applyCloudTemplate(cloud, dream);
-		});
-	    e.stopPropagation();
-	});
-};
-
-var unbindClouds = function() {
-	$('#clouds').unbind("click");
-};
+Cloud.buildCreator(800);
+Cloud.buildCreator(1000);
+Cloud.buildCreator(1200);
 
 $('#novo-sonho').unbind('click').click(function(e) {
     $('#new-cloud').addClass('active-cloud');
@@ -108,20 +29,19 @@ $(document).on('click',function(e) {
 			'-o-animation' : 'moveclouds ' + moveSpeed + 's linear infinite',
 			'animation' : 'moveclouds ' + moveSpeed + 's linear infinite'
 		});
-	    bindClouds();
+	    Cloud.bind();
 	}
     $('.thinking').fadeOut();
 });
 
-bindClouds();
 var params = window.location.search;
 if (params) {
 	var entry = params.split('=');
 	if (entry[0] == '?id') {
 		var id = entry[1].replace('/', "");
-		var cloud = $(".cloud:eq(1)");
+		var cloud = Cloud.create();
 		$.getJSON('/api/dreams/' + id).done(function(dream) {
-			applyCloudTemplate(cloud, dream);
+			Cloud.open(cloud, dream);
 		});
 	}
 }
