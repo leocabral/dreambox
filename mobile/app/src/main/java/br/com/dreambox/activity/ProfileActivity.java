086fc5abc957f2e453514f3eff2d14ca52add469
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-01-30 08:36:52 -0200

    Profile Activity

--- mobile/app/src/main/java/br/com/dreambox/activity/ProfileActivity.java
+++ mobile/app/src/main/java/br/com/dreambox/activity/ProfileActivity.java
@@ -1,31 +1,29 @@
 package br.com.dreambox.activity;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.gson.JsonArray;
 import com.mikhaellopez.circularimageview.CircularImageView;
 
 import br.com.dreambox.R;
-import br.com.dreambox.adapter.DreamAdapter;
-import br.com.dreambox.api.DreamboxApi;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class ProfileActivity extends AppCompatActivity {
-
-    private DreamAdapter dreamAdapter;
 
     @Bind(R.id.img_avatar)
     CircularImageView imgAvatar;
 
     @Bind(R.id.user_nickname)
     TextView userNickname;
+
+    @Bind(R.id.user_mail)
+    TextView userMail;
+
 
     @Bind(R.id.user_location_age)
     TextView userLocationAge;
@@ -41,13 +39,20 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.bind(this);
+        bindProfile();
     }
 
     private void bindProfile() {
-
-
-
+        imgAvatar.setImageResource(R.drawable.avatar_12);
+        userNickname.setText("Mario");
+        userMail.setText("leonardo@gessmail.com");
+        userLocationAge.setText("9 anos, Campinas - SP");
+        userFoundation.setText("Fundação do Sonhador");
+        editProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Editar", Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-
 }
